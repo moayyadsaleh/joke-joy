f1fea44f42e8310ed6b820d0b659e9ebb549082a
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-07-30 14:55:19 -0600

    fix some bugs, add more styling. Run the app

--- index.js
+++ index.js
@@ -1,48 +1,60 @@
+// Import required modules
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+
+// Create an Express app
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+// Set up middleware
+app.use(express.static("public")); // Serve static files from the "public" directory
+app.use(bodyParser.urlencoded({ extended: true })); // Parse incoming URL-encoded data
+app.set("view engine", "ejs"); // Set the view engine to EJS for rendering templates
 
+// API URL for fetching jokes from JokeAPI
+const API_URL = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 
-const API_URL = "https://v2.jokeapi.dev/joke/Any";
-
+// Handle GET request on the root route ("/")
 app.get("/", async (req, res) => {
-    try {
-      const response = await axios.get(AP);
-      const result = response.data;
-      console.log(result);
-      res.render("index.ejs", { data: result });
-    } catch (error) {
-      console.error("Failed to make request:", error.message);
-      res.render("index.ejs", {
-        error: error.message,
-      });
-    }
-  });
+  try {
+    // Fetch a joke from the JokeAPI
+    const response = await axios.get(API_URL);
+    const result = response.data;
+    console.log(result); // Log the fetched joke for debugging purposes
 
-  app.post("/", async (req, res) => {
-    try {
-      // Fetch a new joke from the API without using the user input
-      const response = await axios.get(API_URL);
-      const joke = response.data;
-  
-      // Render the template with the new joke data
-      res.render("index.ejs", {
-        data: joke, // Sending the fetched joke data to the template
-      });
-    } catch (error) {
-      console.error("Failed to make request:", error.message);
-      res.render("index.ejs", {
-        error: "Sorry, No joke now. Please try again later.",
-      });
-    }
-  });
+    // Render the "index.ejs" template with the fetched joke data
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    // If an error occurs during the fetch, render the "index.ejs" template with an error message
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
 
+// Handle POST request on the root route ("/")
+app.post("/", async (req, res) => {
+  try {
+    // Fetch a new joke from the JokeAPI
+    const response = await axios.get(API_URL);
+    const joke = response.data;
+
+    // Render the "index.ejs" template with the newly fetched joke data
+    res.render("index.ejs", {
+      data: joke, // Sending the fetched joke data to the template
+    });
+  } catch (error) {
+    // If an error occurs during the fetch, render the "index.ejs" template with an error message
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "Sorry, No joke now. Please try again later.",
+    });
+  }
+});
+
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
