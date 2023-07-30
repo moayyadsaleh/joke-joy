bc1f752333c923083506248deb0885919aec3279
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-07-30 17:32:03 -0600

    fix bugs and add more features and more styling

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // Parse incoming URL-encode
 app.set("view engine", "ejs"); // Set the view engine to EJS for rendering templates
 
 // API URL for fetching jokes from JokeAPI
-const API_URL = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+const API_URL = "https://v2.jokeapi.dev/joke/Any";
 
 // Handle GET request on the root route ("/")
 app.get("/", async (req, res) => {
@@ -36,23 +36,34 @@ app.get("/", async (req, res) => {
 
 // Handle POST request on the root route ("/")
 app.post("/", async (req, res) => {
-  try {
-    // Fetch a new joke from the JokeAPI
-    const response = await axios.get(API_URL);
-    const joke = response.data;
-
-    // Render the "index.ejs" template with the newly fetched joke data
-    res.render("index.ejs", {
-      data: joke, // Sending the fetched joke data to the template
-    });
-  } catch (error) {
-    // If an error occurs during the fetch, render the "index.ejs" template with an error message
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: "Sorry, No joke now. Please try again later.",
-    });
-  }
-});
+    try {
+      // Extract the selected category from the request body
+      const { type } = req.body;
+  
+      // Update the API URL to include the selected category
+      const API_URL = `https://v2.jokeapi.dev/joke/${type}`;
+      // Fetch a joke from the JokeAPI based on the selected category
+      const response = await axios.get(API_URL);
+      const joke = response.data;
+  
+      // Render the "index.ejs" template with the newly fetched joke data
+      res.render("index.ejs", {
+        data: joke, // Sending the fetched joke data to the template
+      });
+    } catch (error) {
+      // If an error occurs during the fetch, render the "index.ejs" template with an error message
+      console.error("Failed to make request:", error.message);
+      res.render("index.ejs", {
+        error: "Sorry, No joke now. Please try again later.",
+      });
+    }
+  });
+  
+  
+  
+  
+  
+  
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
