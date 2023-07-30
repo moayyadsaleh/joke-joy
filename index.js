bc707fc01feb73c01dbc4f839c8c43b382dc219c
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-07-30 11:44:36 -0600

    add required dependencies. Start local server

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from"body-parser";
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs'); 
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { content: "Waiting for data..." });
+  });
+  
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
